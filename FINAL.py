fe81760e27fbda117580614f79d144e5be693497
OmarEnriqueVivasEyrich <oevivaseyrich@gmail.com>
2024-11-28 19:43:51 -0500

    Update FINAL.py

--- FINAL.py
+++ FINAL.py
@@ -5,7 +5,7 @@ import streamlit as st
 import os
 
 # Definir la ruta del archivo Parquet
-file_path = 'DatosParquet_reducido.parquet'  # Cambiado a ruta relativa
+file_path = 'parquet.parquet'  # Cambiado a ruta relativa
 
 # Configuración de estilo
 st.set_page_config(page_title="Dashboard de Puntajes y Estratos", layout="wide")
